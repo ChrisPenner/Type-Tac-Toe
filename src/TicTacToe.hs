5e33f521ad640512e3cc3f92f2ec75c49ab28672
Chris Penner <christopher.penner@gmail.com>
2017-08-20 18:37:27 -0600

    No more proxy's

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -8,21 +8,28 @@
 module TicTacToe where
 
 import Data.Function ((&))
-import Data.Proxy
 import GHC.TypeLits
 import Linear.V3
 import Linear.Vector
--- import Data.Functor.Rep
--- import Data.Distributive
 import Data.Bifunctor
 
 -- | Keep a list of each Piece played and its location
 data BoardRep = Empty
-              | Cons CoordT CoordT Piece BoardRep
+              | Cons CoordT CoordT PieceT BoardRep
 
 -- | Either X, O, or Nothing
-data Piece = X | O | N
+data PieceT = X | O | N
   deriving (Show, Eq)
+
+data Piece a where
+  X' :: Piece X
+  O' :: Piece O
+  N' :: Piece N
+
+pieceT :: Piece a -> PieceT
+pieceT X' = X
+pieceT O' = O
+pieceT N' = N
 
 data CoordT = A | B | C
   deriving (Show, Eq)
@@ -32,18 +39,18 @@ data Coord (a :: CoordT) where
   B' :: Coord B
   C' :: Coord C
 
-toVal :: Coord a -> CoordT
-toVal A' = A
-toVal B' = B
-toVal C' = C
+coordT :: Coord a -> CoordT
+coordT A' = A
+coordT B' = B
+coordT C' = C
 
 -- | Is it the proper turn for p to play a piece?
-type family IsTurn (p :: Piece) (b :: BoardRep) where
+type family IsTurn (p :: PieceT) (b :: BoardRep) where
   IsTurn X b = Count X b ~ Count O b
   IsTurn O b = Count X b ~ (Count O b + 1)
 
 -- | Count amount of a certain piece on the board
-type family Count (t :: Piece) (l :: BoardRep) :: Nat where
+type family Count (t :: PieceT) (l :: BoardRep) :: Nat where
   Count _ Empty = 0
   Count t (Cons _ _ t rest) = 1 + Count t rest
   Count t (Cons _ _ _ rest) = Count t rest
@@ -72,28 +79,18 @@ indexVector A (V3 a _ _) = a
 indexVector B (V3 _ a _) = a
 indexVector C (V3 _ _ a) = a
 
-newBoard :: Board Empty Piece
+newBoard :: Board Empty PieceT
 newBoard = Board $ V3 (V3 N N N)
                       (V3 N N N)
                       (V3 N N N)
 
-class APiece x where
-  pieceVal :: Proxy x -> Piece
-
-instance APiece X where
-  pieceVal _ = X
-
-instance APiece O where
-  pieceVal _ = O
-
 -- | Play a piece on square (m, n) if it's valid to do so
 play ::
   ( IsTurn p b
-  , APiece p
   , Played m n b ~ False
-  ) => Proxy (p :: Piece) -> (Coord m, Coord n) -> Board b Piece -> Board (Cons m n p b) Piece
+  ) => Piece p -> (Coord m, Coord n) -> Board b PieceT -> Board (Cons m n p b) PieceT
 
-play (pieceVal -> p) (toVal -> x, toVal -> y) b = tabulate go
+play (pieceT -> p) (coordT -> x, coordT -> y) b = tabulate go
   where
     go i
       | i == (x, y) = p
@@ -102,22 +99,19 @@ play (pieceVal -> p) (toVal -> x, toVal -> y) b = tabulate go
 playX ::
   ( IsTurn X b
   , Played m n b ~ False
-  ) => (Coord m, Coord n) -> Board b Piece -> Board (Cons m n X b) Piece
-playX = play (Proxy :: Proxy X)
+  ) => (Coord m, Coord n) -> Board b PieceT -> Board (Cons m n X b) PieceT
+playX = play X'
 
 playO ::
   ( IsTurn O b
   , Played m n b ~ False
-  ) => (Coord m, Coord n) -> Board b Piece -> Board (Cons m n O b) Piece
-playO = play (Proxy :: Proxy O)
-
-p :: Proxy x
-p = Proxy
+  ) => (Coord m, Coord n) -> Board b PieceT -> Board (Cons m n O b) PieceT
+playO = play O'
 
 clearType :: Board b a -> Board () a
 clearType (Board b) = Board b
 
-game :: Board () Piece
+game :: Board () PieceT
 game = newBoard
      & playX (A', B')
      & playO (C', C')
