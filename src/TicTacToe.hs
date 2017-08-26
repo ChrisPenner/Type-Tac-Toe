6a224b120baa2a619c7f3d655b2e4cee17b8b815
Chris Penner <christopher.penner@gmail.com>
2017-08-26 11:00:34 -0600

    Add TicTacToe

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -5,13 +5,21 @@
 {-# language UndecidableInstances #-}
 {-# language ViewPatterns #-}
 {-# language ConstraintKinds #-}
+{-# language GADTs #-}
 module TicTacToe where
 
 import Data.Function ((&))
 import GHC.TypeLits
-import Linear.V3
-import Linear.Vector
-import Data.Bifunctor
+
+data Trip a = Trip a a a
+  deriving (Show, Eq, Functor)
+
+-- | Utility function to alter a value inside a triple
+-- Can build get / set using `flip const ()` and `const x` respectively
+overTrip :: CoordT -> (a -> a) -> Trip a -> Trip a
+overTrip A f (Trip a b c) = Trip (f a) b c
+overTrip B f (Trip a b c) = Trip a (f b) c
+overTrip C f (Trip a b c) = Trip a b (f c)
 
 -- | Keep a list of each Piece played and its location
 data BoardRep = Empty
@@ -21,99 +29,74 @@ data BoardRep = Empty
 data PieceT = X | O | N
   deriving (Show, Eq)
 
+-- | A proxy type which represents a piece
 data Piece a where
-  X' :: Piece X
-  O' :: Piece O
-  N' :: Piece N
+  X' :: Piece 'X
+  O' :: Piece 'O
+  N' :: Piece 'N
 
-pieceT :: Piece a -> PieceT
-pieceT X' = X
-pieceT O' = O
-pieceT N' = N
+-- | Get the piece's actual value from a wrapper type
+pieceVal :: Piece a -> PieceT
+pieceVal X' = X
+pieceVal O' = O
+pieceVal N' = N
 
 data CoordT = A | B | C
   deriving (Show, Eq)
 
+-- | A proxy type which represents a coordinate
 data Coord (a :: CoordT) where
-  A' :: Coord A
-  B' :: Coord B
-  C' :: Coord C
+  A' :: Coord 'A
+  B' :: Coord 'B
+  C' :: Coord 'C
 
-coordT :: Coord a -> CoordT
-coordT A' = A
-coordT B' = B
-coordT C' = C
+-- | Get the coord's actual value from a wrapper type
+coordVal :: Coord a -> CoordT
+coordVal A' = A
+coordVal B' = B
+coordVal C' = C
 
 -- | Is it the proper turn for p to play a piece?
 type family IsTurn (p :: PieceT) (b :: BoardRep) where
-  IsTurn X b = Count X b ~ Count O b
-  IsTurn O b = Count X b ~ (Count O b + 1)
+  IsTurn 'X b = Count 'X b ~ Count 'O b
+  IsTurn 'O b = Count 'X b ~ (Count 'O b + 1)
 
 -- | Count amount of a certain piece on the board
 type family Count (t :: PieceT) (l :: BoardRep) :: Nat where
-  Count _ Empty = 0
-  Count t (Cons _ _ t rest) = 1 + Count t rest
-  Count t (Cons _ _ _ rest) = Count t rest
+  Count _ 'Empty = 0
+  Count t ('Cons _ _ t rest) = 1 + Count t rest
+  Count t ('Cons _ _ _ rest) = Count t rest
 
 -- | Has a square been played already?
 type family Played (m :: CoordT) (n :: CoordT) (l :: BoardRep) :: Bool where
-  Played _ _ Empty = False
-  Played m n (Cons m n _ _) = True
-  Played m n (Cons _ _ _ rest) = Played m n rest
+  Played _ _ 'Empty = 'False
+  Played m n ('Cons m n _ _) = 'True
+  Played m n ('Cons _ _ _ rest) = Played m n rest
 
 -- | A Board is a 3x3 vector
-newtype Board x a = Board (V3 (V3 a))
+newtype Board x a = Board (Trip (Trip a))
   deriving (Functor, Show, Eq)
 
-index :: Board b a -> (CoordT, CoordT) -> a
-index (Board vs) (x, y) = indexVector y $ indexVector x vs
-
-tabulate :: ((CoordT, CoordT) -> a) -> Board b a
-tabulate d = Board (V3 (V3 (d (A, A)) (d (A, B)) (d (A, C)))
-                        (V3 (d (B, A)) (d (B, B)) (d (B, C)))
-                        (V3 (d (C, A)) (d (C, B)) (d (C, C)))
-                   )
-
-indexVector :: CoordT -> V3 a -> a
-indexVector A (V3 a _ _) = a
-indexVector B (V3 _ a _) = a
-indexVector C (V3 _ _ a) = a
-
-newBoard :: Board Empty PieceT
-newBoard = Board $ V3 (V3 N N N)
-                      (V3 N N N)
-                      (V3 N N N)
+-- | New empty board
+newBoard :: Board 'Empty PieceT
+newBoard = Board $ Trip (Trip N N N)
+                      (Trip N N N)
+                      (Trip N N N)
 
 -- | Play a piece on square (m, n) if it's valid to do so
 play ::
   ( IsTurn p b
-  , Played m n b ~ False
-  ) => Piece p -> (Coord m, Coord n) -> Board b PieceT -> Board (Cons m n p b) PieceT
+  , Played m n b ~ 'False
+  ) => Piece p -> (Coord m, Coord n) -> Board b PieceT -> Board ('Cons m n p b) PieceT
+play (pieceVal -> p) (coordVal -> x, coordVal -> y) (Board b) = Board $ overTrip y (overTrip x (const p)) b
 
-play (pieceT -> p) (coordT -> x, coordT -> y) b = tabulate go
-  where
-    go i
-      | i == (x, y) = p
-      | otherwise = index b i
-
-playX ::
-  ( IsTurn X b
-  , Played m n b ~ False
-  ) => (Coord m, Coord n) -> Board b PieceT -> Board (Cons m n X b) PieceT
-playX = play X'
-
-playO ::
-  ( IsTurn O b
-  , Played m n b ~ False
-  ) => (Coord m, Coord n) -> Board b PieceT -> Board (Cons m n O b) PieceT
-playO = play O'
-
+-- | Clear any associated type info about the board
 clearType :: Board b a -> Board () a
 clearType (Board b) = Board b
 
 game :: Board () PieceT
 game = newBoard
-     & playX (A', B')
-     & playO (C', C')
-     & playX (A', A')
+     & play X' (A', B')
+     & play O' (C', C')
+     & play X' (A', A')
      & clearType
